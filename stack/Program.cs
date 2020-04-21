b3bd452a64c2af6bd06351ae0d4c08f7ea58f725
jonatan.maxi.ponce@gmail.com <jonatan.maxi.ponce@gmail.com>
2020-04-21 15:15:11 -0300

    Declaracion de una Stack Agregado de elementos

---
+++ stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de Stack
+            Stack<string> nombres = new Stack<string>();
+
+            //Agregamos elementos a la Stack
+            nombres.Push("martin");
+            nombres.Push("jonah");
+            nombres.Push("yesica");
+            nombres.Push("rosario");
+            nombres.Push("sebas");
+
+            //Recorremos la pila
+        }
+    }
+}
